abe8156c853b5777f5c5b8134b0f1bbee9efbae8
180abromotor@gmail.com <180abromotor@gmail.com>
2025-12-31 17:09:31 +0100

    logouot añadido

--- backend/src/routes/autenticacion.routes.js
+++ backend/src/routes/autenticacion.routes.js
@@ -4,4 +4,18 @@ const { login } = require('../controllers/autenticacion.controller');
 
 router.post('/login', login);
 
+router.post("/logout", (req, res) => {
+  res.clearCookie("token", {
+    httpOnly: true,
+    sameSite: "lax",
+    secure: process.env.NODE_ENV === "production",
+    path: "/", // 🔴 MUY IMPORTANTE
+  });
+
+  res.json({
+    success: true,
+    message: "Logout correcto",
+  });
+});
+
 module.exports = router;
